d9325b08dfa4429f4101cd5db06f287405cf4d4c
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-05-18 16:07:15 -0400

    oops

--- cpp/Syntax.hx
+++ cpp/Syntax.hx
@@ -4,7 +4,7 @@ import haxe.exceptions.NotImplementedException;
 
 extern class Syntax {
     public extern overload inline static function code(code:String) {
-        untyped __cpp__(code, args);
+        untyped __cpp__(code);
     }
     public extern overload inline static function code(code:String, args:haxe.extern.Rest<Dynamic>) {
         throw new NotImplementedException();
