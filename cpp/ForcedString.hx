a82fc4171d616a031bc6da86e3efb4d0cc4455e9
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-06-01 17:42:47 -0400

    Add ForcedString (FUCK YOU HXCPP)

---
+++ cpp/ForcedString.hx
@@ -0,0 +1,21 @@
+package cpp;
+
+/**
+ * Will force a cast to `::String` when compiled
+ * 
+ * 
+ * FUCK YOU HXCPP
+ * WHY DO YOU TRACE TRUE WHEN TRACING A CHAR??
+ * WHAT THE FUCK???
+ */
+extern abstract ForcedString(String) to (String) {
+	inline function new(s:String)
+        this = untyped __cpp__("::String( {0} )", s);
+
+	@:from
+	static public inline function fromString(s:String):ForcedString
+		return new ForcedString(s);
+
+	@:to extern public inline function toString():String
+		return new String(untyped this);
+}
