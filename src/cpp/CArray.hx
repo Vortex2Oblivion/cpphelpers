94c1e149e82f1250920e3d8036d9e14b532986c6
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-12-24 19:28:02 -0500

    change version

--- src/cpp/CArray.hx
+++ src/cpp/CArray.hx
@@ -4,7 +4,7 @@ extern class CArray {
 	/*inline static function create<T>(array:Array<T>) {
 		return untyped __cpp__(" (( int *) (cpp::Pointer_obj::ofArray( {1} ).value) )", "int", array);
 	}*/
-	inline static function create<T>(size:SizeT, type:Any):RawPointer<T> {
+	inline static function create<T>(size:SizeT, type:Class<Any>):RawPointer<T> {
 		return untyped __cpp__("(PyMethodDef*)malloc(sizeof({0}) * {1})", type, size);
 	}
 }
