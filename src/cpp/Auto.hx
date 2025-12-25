f4cf26bdfa71bcc41346928c7c0bf815a26f80f3
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-12-24 19:12:14 -0500

    change

---
+++ src/cpp/Auto.hx
@@ -0,0 +1,8 @@
+package cpp;
+
+@:native("auto")
+@:arrayAccess
+@:nativeArrayAccess
+extern class Auto implements ArrayAccess<Dynamic> {
+    
+}
--- src/cpp/CArray.hx
+++ src/cpp/CArray.hx
@@ -1,7 +1,10 @@
 package cpp;
 
 extern class CArray {
-    extern overload inline static function create<T>(array:Array<T>) {
-        return untyped __cpp__(" (( int *) (cpp::Pointer_obj::ofArray( {1} ).value) )", "int", array);
-    }
+	/*inline static function create<T>(array:Array<T>) {
+		return untyped __cpp__(" (( int *) (cpp::Pointer_obj::ofArray( {1} ).value) )", "int", array);
+	}*/
+	inline static function create<T>(size:SizeT, type:Any):RawPointer<T> {
+		return untyped __cpp__("(PyMethodDef*)malloc(sizeof({0}) * {1})", type, size);
+	}
 }
